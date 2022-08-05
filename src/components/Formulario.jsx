197c677ab7bcb129a20c86046e4537458e2520b1
Omar Noyola <omis1296@gmail.com>
2022-08-05 11:49:29 -0500

    :memo: Variables de Entorno

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -29,7 +29,7 @@ const Formulario = ({cliente, cargando}) => {
 
             if(cliente.id){
                 //Editando
-                const url = `http://localhost:4000/clientes/${cliente.id}`;
+                const url = `${import.meta.env.VITE_API_URL}/${cliente.id}`;
                 respuesta = await fetch(url, {
                     method: 'PUT',
                     body: JSON.stringify(valores),
@@ -40,7 +40,7 @@ const Formulario = ({cliente, cargando}) => {
             }
             else{
                 //Nuevo registro
-                const url = 'http://localhost:4000/clientes';
+                const url = import.meta.env.VITE_API_URL;
                 respuesta = await fetch(url, {
                     method: 'POST',
                     body: JSON.stringify(valores),
